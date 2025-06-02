305c5ae75ba90e2a57ea089a64f7662c47fb1c94
Jasser Houimli <houimlijasser9@email.com>
2025-06-02 12:40:17 +0100

    Refactors DTO'S

---
+++ src/backend/DTOs/Google/Gmail.cs
@@ -0,0 +1,10 @@
+
+public class EmailMessageDto
+{
+    public string Id { get; set; } = string.Empty;
+    public string Subject { get; set; } = string.Empty;
+    public string From { get; set; } = string.Empty;
+    public string Date { get; set; } = string.Empty;
+    public string Snippet { get; set; } = string.Empty;
+    public string? Body { get; set; }
+}
---
+++ src/backend/DTOs/Google/GoogleUserInfo.cs
@@ -0,0 +1,11 @@
+namespace backend.DTOs ;
+
+public sealed record GoogleUserInfo
+{
+    public string Id { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Picture { get; set; } = string.Empty;
+    public string GivenName { get; set; } = string.Empty;
+    public string FamilyName { get; set; } = string.Empty;
+}
